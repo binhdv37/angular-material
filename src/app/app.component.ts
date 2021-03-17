67522f6c8c16316b78086fc554a82cfa0b6aa25e
binhdv <binhdv@dft.vn>
2021-03-17 17:41:24 +0700

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'learnMaterial';
+export class AppComponent implements OnInit{
+
+  public usersForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.usersForm = this.fb.group({
+      date: this.fb.control(new Date()),
+      permissions: this.fb.array([
+        this.fb.control(''),
+        this.fb.control(''),
+        this.fb.control(''),
+        this.fb.control(''),
+      ])
+    });
+  }
+
+  get permissions(): FormArray {
+    return this.usersForm.get('permissions') as FormArray;
+  }
+
+  onSubmit() {
+    console.log(this.usersForm.value);
+  }
+
 }
